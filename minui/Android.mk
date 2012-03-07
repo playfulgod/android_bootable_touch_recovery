e91f2eea7fc036205b77ce626800fe12aabd09b0
gweedo767 <njgreb@gmail.com>
2012-03-07 14:20:13 -0800

    first commit

---
+++ minui/Android.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := events.c resources.c
+ifneq ($(BOARD_CUSTOM_GRAPHICS),)
+  LOCAL_SRC_FILES += $(BOARD_CUSTOM_GRAPHICS)
+else
+  LOCAL_SRC_FILES += graphics.c
+endif
+
+LOCAL_C_INCLUDES +=\
+    external/libpng\
+    external/zlib
+
+LOCAL_MODULE := libminui
+
+ifeq ($(TARGET_RECOVERY_PIXEL_FORMAT),"RGBX_8888")
+  LOCAL_CFLAGS += -DRECOVERY_RGBX
+endif
+ifeq ($(TARGET_RECOVERY_PIXEL_FORMAT),"BGRA_8888")
+  LOCAL_CFLAGS += -DRECOVERY_BGRA
+endif
+
+ifeq ($(BOARD_XDPI_RECOVERY),true)
+  LOCAL_CFLAGS += -DBOARD_XDPI_RECOVERY
+else
+ifeq ($(BOARD_LDPI_RECOVERY),true)
+  LOCAL_CFLAGS += -DBOARD_LDPI_RECOVERY
+else
+  LOCAL_CFLAGS += -DBOARD_MDPI_RECOVERY
+endif
+endif
+
+include $(BUILD_STATIC_LIBRARY)
